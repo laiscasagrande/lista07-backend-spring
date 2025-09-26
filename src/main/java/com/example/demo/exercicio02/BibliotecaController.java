e1ffe6addef1a5ee499a5aa5ffea84818dfc8834
Laís <laisluanaka@outlook.com>
2025-09-26 10:59:27 -0300

    finalização do exercício 02

--- src/main/java/com/example/demo/exercicio02/BibliotecaController.java
+++ src/main/java/com/example/demo/exercicio02/BibliotecaController.java
@@ -1,5 +1,40 @@
 package com.example.demo.exercicio02;
 
-public class BibliotecaController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/biblioteca")
+public class BibliotecaController {
+    private final BibliotecaService bibliotecaService;
+
+    public BibliotecaController(BibliotecaService bibliotecaService) {
+        this.bibliotecaService = bibliotecaService;
+    }
+
+    @GetMapping("/livros")
+    public List<Livro> exibeLivros() {
+        return bibliotecaService.getLivrosDisponiveis();
+    }
+
+    @GetMapping("/emprestados")
+    public List<Emprestimo> exibeReservas() {
+        return bibliotecaService.getEmprestimos();
+    }
+
+    @PostMapping("/emprestados/{id}/{usuarioId}")
+    public Emprestimo criarEmprestimo(UUID id, String usuarioId) {
+        return bibliotecaService.criarEmprestimo(id, usuarioId);
+    }
+
+    @DeleteMapping("/emprestados/{emprestimoId}")
+    public void removerEmprestimo(UUID emprestimoId) {
+        bibliotecaService.removerEmprestimo(emprestimoId);
+    }
 }
---
+++ src/main/java/com/example/demo/exercicio02/DemoApplicationBiblioteca.java
@@ -0,0 +1,14 @@
+package com.example.demo.exercicio02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplicationBiblioteca {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplicationBiblioteca.class, args);
+	}
+
+}
